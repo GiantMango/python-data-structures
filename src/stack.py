12b39e8753d3579a918d4982bb01a92e559ae1c1
Giant Mango <subway1101@gmail.com>
2026-06-04 22:10:23 -0700

    [feat] add stack implementation

--- src/stack.py
+++ src/stack.py
@@ -1,8 +1,11 @@
 # Linked list
 class Node:
-    def __init__(self, value, next):
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
+
+    def __str__(self):
+        return f"Top: {str(self.value)}"
 
 
 class Stack:
@@ -10,6 +13,53 @@ class Stack:
         self.size = 0
         self.top = None
 
+    def push(self, value):
+        new_node = Node(value, self.top)
+        self.top = new_node
+        self.size += 1
 
-stack = Stack()
-print(stack)
+    def pop(self):
+        if self.size == 0:
+            raise IndexError("pop from empty stack")
+        removed = self.top
+        self.top = self.top.next
+        self.size -= 1
+        return removed
+
+    def peek(self):
+        if self.size == 0:
+            raise IndexError("peek from empty stack")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+    def contains(self, value):
+        return value in self
+
+    def __contains__(self, value):
+        current = self.top
+
+        while current:
+
+            if value == current.value:
+                return True
+            current = current.next
+
+        return False
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        if self.is_empty():
+            return "Empty"
+
+        values = ["top"]
+        current = self.top
+
+        while current:
+            values.append(str(current.value))
+            current = current.next
+        values.append("None")
+        return " -> ".join(values)
