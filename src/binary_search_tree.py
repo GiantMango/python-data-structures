c445a9974efbbbe8d305a085866c0e0bdb135452
Giant Mango <subway1101@gmail.com>
2026-06-08 22:52:32 -0700

    [feat] add binary search tree file

---
+++ src/binary_search_tree.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class BinarySearchTree:
+    def __init__(self):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def delete(self, value):
+        pass
+
+    def traversal(self):
+        pass
+
+    def __contains__(self, value):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
