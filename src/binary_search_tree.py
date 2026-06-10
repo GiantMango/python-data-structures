4d2e406b6054add92d76959d102ff3ee825d9a7b
Giant Mango <subway1101@gmail.com>
2026-06-09 21:19:09 -0700

    [feat] add delete

--- src/binary_search_tree.py
+++ src/binary_search_tree.py
@@ -18,7 +18,10 @@ class Node:
         self.right = None
 
     def __str__(self):
-        return self.value
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
 
 
 class BinarySearchTree:
@@ -59,8 +62,24 @@ class BinarySearchTree:
         if self.size == 0:
             raise IndexError("delete from empty tree")
 
-        current = self.root
-        parent = self.root
+        parent, current = self._find_node_and_parent(value)
+
+        if current.left is None and current.right is None:  # 1. deleting a leaf node
+            self._replace_parent_child(parent, current, None)
+        elif current.left is None:  # 2. deleting a node with 1 child on the right
+            self._replace_parent_child(parent, current, current.right)
+        elif current.right is None:  # 2. deleting a node with 1 child on the left
+            self._replace_parent_child(parent, current, current.left)
+        else:  # 3. deleting a node with 2 children
+            new_child = self._find_min_node(current.right)
+            new_parent, _ = self._find_node_and_parent(new_child.value)
+            current.value = new_child.value
+            if new_parent.right == new_child:
+                new_parent.right = new_child.right
+            elif new_parent.left == new_child:
+                new_parent.left = new_child.right
+
+        self.size -= 1
 
     def in_order_traversal(self):
         """
@@ -162,11 +181,45 @@ class BinarySearchTree:
             current = current.right
         return current
 
+    def _find_node_and_parent(self, value):
+        current = self.root
+        parent = None
+
+        while current is not None:
+            if value == current.value:
+                return (parent, current)
+            elif value > current.value:
+                parent = current
+                current = current.right
+            else:
+                parent = current
+                current = current.left
+
+        raise KeyError(value)
+
+    def _replace_parent_child(self, parent, current, new):
+        if current == self.root:
+            self.root = new
+            return
+        if parent.right == current:
+            parent.right = new
+        elif parent.left == current:
+            parent.left = new
+
     def _height(self):
         raise NotImplementedError
 
     def __contains__(self, value):
-        raise NotImplementedError
+        current = self.root
+
+        while current is not None:
+            if value == current.value:
+                return True
+            elif value > current.value:
+                current = current.right
+            else:
+                current = current.left
+        return False
 
     def __len__(self):
         return self.size
@@ -175,7 +228,6 @@ class BinarySearchTree:
         if self.root is None:
             return "Empty"
 
-        current = self.root
         values = []
         return "->".join(values)
 
@@ -185,13 +237,18 @@ if __name__ == "__main__":
     bst.insert(10)
     bst.insert(5)
     bst.insert(15)
-    bst.insert(3)
     bst.insert(7)
+    bst.insert(3)
     bst.insert(12)
     bst.insert(20)
+    bst.insert(13)
     bst.insert(8)
+    print(bst.level_order_traversal())
 
-    print(bst.in_order_traversal())
-    print(bst.pre_order_traversal())
-    print(bst.post_order_traversal())
+    bst.delete(10)
+    print(bst.level_order_traversal())
+    bst.delete(5)
+
+    print(bst.level_order_traversal())
+    bst.delete(15)
     print(bst.level_order_traversal())
